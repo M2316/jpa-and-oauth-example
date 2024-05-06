fb2713a731aee112c21d8d27c889952a77a36a8d
M2316 <031pjs@gmail.com>
2024-05-06 10:22:52 +0900

    jwt를 이용한 토큰 기반의 인증 처리 - 완료

---
+++ src/main/java/site/jealth/oauth/springbootoauth2exaple/config/jwt/JwtProperties.java
@@ -0,0 +1,16 @@
+package site.jealth.oauth.springbootoauth2exaple.config.jwt;
+
+import lombok.Data;
+import org.hibernate.annotations.Comment;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@Configuration
+@ConfigurationProperties("jwt") //자바 클래스에 프로퍼티값을 가져와서 사용하는 애너테이션
+public class JwtProperties {
+    private String issuer;
+    private String secretKey;
+}
